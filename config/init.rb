fe8ec8a4c07c37b7d4cca4aff4bfc0970ba97b1b
Daniel Parker <dcparker@gmail.com>
2009-08-11 20:23:56 -0400

    Added static public directory.

--- config/init.rb
+++ config/init.rb
@@ -17,4 +17,9 @@ require_all 'app/models/*.rb'
 # Load Controllers IF Sinatra is loaded.
 # This way you can require this file (and therefore the entire app) in irb,
 # and it won't the controllers by default.
-require_all 'app/controllers/*.rb' if ::Object.const_defined?(:Sinatra)
+if ::Object.const_defined?(:Sinatra)
+  Sinatra::Application.set :public, 'public'
+  Sinatra::Application.set :static, true
+  require_all 'app/controllers/*.rb'
+end
+
