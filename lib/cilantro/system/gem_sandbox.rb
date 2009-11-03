0299952459091f91e445aa849bf294445ec97ecb
dcparker <dcparker@gmail.com>
2009-11-03 02:48:15 -0500

    Fixed up further for heroku.

--- lib/cilantro/system/gem_sandbox.rb
+++ lib/cilantro/system/gem_sandbox.rb
@@ -8,6 +8,12 @@ APP_ROOT = File.expand_path(Dir.pwd) unless defined?(APP_ROOT)
 require 'rubygems'
 require 'rubygems/custom_require'
 
+module Kernel
+  def require_with_auto_install(name, options={})
+    require name
+  end
+end
+
 if File.exists?("#{APP_ROOT}/gems") && File.writable?("#{APP_ROOT}/gems")
   # Oh but first, go ahead and install any missing gems (PLEASE, only include gems/specifications and gems/cache in your git repo)
     require File.dirname(__FILE__) + '/install_missing_gems'
