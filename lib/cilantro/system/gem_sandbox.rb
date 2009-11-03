3abc9f4585474ead02f195ce5305dc4a2bd7dc5c
dcparker <dcparker@gmail.com>
2009-11-03 02:46:32 -0500

    Fixing up gem handling for heroku.

--- lib/cilantro/system/gem_sandbox.rb
+++ lib/cilantro/system/gem_sandbox.rb
@@ -8,7 +8,7 @@ APP_ROOT = File.expand_path(Dir.pwd) unless defined?(APP_ROOT)
 require 'rubygems'
 require 'rubygems/custom_require'
 
-if File.exists?("#{APP_ROOT}/gems")
+if File.exists?("#{APP_ROOT}/gems") && File.writable?("#{APP_ROOT}/gems")
   # Oh but first, go ahead and install any missing gems (PLEASE, only include gems/specifications and gems/cache in your git repo)
     require File.dirname(__FILE__) + '/install_missing_gems'
   Gem.use_paths("#{APP_ROOT}/gems", ["#{APP_ROOT}/gems"])
--- lib/cilantro/system/install_missing_gems.rb
+++ lib/cilantro/system/install_missing_gems.rb
@@ -15,16 +15,20 @@ if File.exists?(gempath)
   # Redirect standard output to the netherworld
   no_debug = '2>&1 >/dev/null'
 
+  all_gems = ""
   # Ensure each gem in gems/cache is installed
   Dir.glob("#{gempath}/cache/*.gem").each do |gem_name|
     gem_name = gem_name.match(/([^\/]+)\.gem/)[1]
+    j, name, version = *gem_name.match(/^(.*)-([\d\.]+)$/)
+    all_gems << "#{name} --version '#{version}'\n"
     if !File.exists?("#{gempath}/gems/#{gem_name}")
       puts "Installing gem: #{gem_name}"
-      j, name, version = *gem_name.match(/^(.*)-([\d\.]+)$/)
       puts `gem pristine --config-file gems/gemrc.yml #{name} -v#{version}`
       # LEAVE THIS HERE FOR LATER REFERENCE - These two commands unpack gems folders.
       # `mkdir -p #{gempath}/gems/#{gem_name} #{no_debug}`
       # `tar -Oxf #{gempath}/cache/#{gem_name}.gem data.tar.gz | tar -zx -C #{gempath}/gems/#{gem_name}/ #{no_debug}`
     end
   end
+  # Writes a .gems file for use on sites such as heroku, that auto-install your gems.
+  open(".gems", 'w') {|f| f << all_gems }
 end
