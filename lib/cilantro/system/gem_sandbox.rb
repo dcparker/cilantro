faa6025fb4352f77b54c2c6ca41f43e2b7050174
dcparker <dcparker@gmail.com>
2009-11-17 21:42:31 -0500

    Fixed the gem auto-install a bit.

--- lib/cilantro/system/gem_sandbox.rb
+++ lib/cilantro/system/gem_sandbox.rb
@@ -7,6 +7,7 @@ APP_ROOT = File.expand_path(Dir.pwd) unless defined?(APP_ROOT)
 
 require 'rubygems'
 require 'rubygems/custom_require'
+require File.dirname(__FILE__) + '/install_missing_gems'
 
 module Kernel
   def require_with_auto_install(name, options={})
@@ -16,6 +17,6 @@ end
 
 if File.exists?("#{APP_ROOT}/gems") && File.writable?("#{APP_ROOT}/gems")
   # Oh but first, go ahead and install any missing gems (PLEASE, only include gems/specifications and gems/cache in your git repo)
-    require File.dirname(__FILE__) + '/install_missing_gems'
+    Cilantro.install_missing_gems
   Gem.use_paths("#{APP_ROOT}/gems", ["#{APP_ROOT}/gems"])
 end
