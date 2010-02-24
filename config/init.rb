7f2e7619f73a34f2663b61449a0ef19393a88785
dcparker <dcparker@gmail.com>
2010-02-24 00:09:19 -0500

    Added static files, init.rb, and a bunch of gems to starter app.

---
+++ config/init.rb
@@ -0,0 +1,39 @@
+# File: config/init.rb
+# Sets up configurations and includes gems, libraries, controllers & models.
+# To start the web server, use bin/cilantro.
+# 
+# To be loaded ONLY by Cilantro.load_environment. These things are already done at this point:
+#   + ./lib is included in the ruby load path
+#   + rubygems is loaded, but sandboxed to ./gems if ./gems exists
+#   + if we're running in server or test mode, sinatra has been loaded
+
+
+###################
+# Section: Options
+# Set your Cilantro options here.
+Cilantro.auto_reload = true
+
+
+###################
+# Section: Dependencies and Libraries
+require 'cilantro/templater'
+require 'openssl'
+require 'base64'
+require 'cgi'
+dependency 'json'
+
+
+###################
+# Section: Database Setup
+dependency 'sqlite3', :gem => 'sqlite3-ruby', :env => :development
+dependency 'do_sqlite3', :env => :development
+dependency 'do_mysql', :env => :production
+dependency 'dm-core'
+dependency 'data_objects'
+dependency 'dm-types'
+dependency 'dm-migrations'
+# dependency 'dm-validations'
+
+# Uncomment this to fire up a connection to the database using settings from config/database.yml config
+# It's configured for DataMapper by default, you can set up your own connection routine here instead.
+Cilantro.setup_database
